2ec4a372c9c79de3d6a66056e68133cb2221237d
miltondev <miltondev@outlook.com>
2017-11-05 21:41:21 -0600

    first commit

---
+++ UploadFiles/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UploadFiles.Models;
+
+namespace UploadFiles.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SaveUploadedFile()
+        {
+            bool isSavedSuccessfully = true;
+            string fName = "";
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+                //Save file content goes here
+                fName = file.FileName;
+                if (file != null && file.ContentLength > 0)
+                {
+
+                    var originalDirectory = new DirectoryInfo(string.Format("{0}UploadFiles\\Files", Server.MapPath(@"\")));
+
+                    string pathString = System.IO.Path.Combine(originalDirectory.ToString(), "Filepath");
+
+                    var fileName1 = Path.GetFileName(file.FileName);
+
+
+                    bool isExists = System.IO.Directory.Exists(pathString);
+
+                    if (!isExists)
+                        System.IO.Directory.CreateDirectory(pathString);
+
+                    var path = string.Format("{0}\\{1}", pathString, file.FileName);
+                    file.SaveAs(path);
+
+                }
+
+            }
+
+            if (isSavedSuccessfully)
+            {
+                return Json(new { Message = fName });
+            }
+            else
+            {
+                return Json(new { Message = "Error in saving file" });
+            }
+        }
+
+
+
+
+
+    }
+}
